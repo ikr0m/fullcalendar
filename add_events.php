a54bad054db0647f114cc70fab7ac6de7d85e1b0
Ikrom <ikrom@tocobox.com>
2016-02-01 23:36:52 +0500

    reformat code, added lib folder

--- add_events.php
+++ add_events.php
@@ -6,13 +6,13 @@ $end = $_POST['end'];
 
 // connection to the database
 try {
-$bdd = new PDO('mysql:host=localhost;dbname=fullcalendar', 'root', '');
-} catch(Exception $e) {
-exit('Unable to connect to database.');
+    $bdd = new PDO('mysql:host=localhost;dbname=fullcalendar', 'root', '');
+} catch (Exception $e) {
+    exit('Unable to connect to database.');
 }
 
 // insert the records
 $sql = "INSERT INTO evenement (title, start, end) VALUES (:title, :start, :end )";
 $q = $bdd->prepare($sql);
-$q->execute(array(':title'=>$title, ':start'=>$start, ':end'=>$end));
+$q->execute(array(':title' => $title, ':start' => $start, ':end' => $end));
 ?>
--- update_events.php
+++ update_events.php
@@ -8,12 +8,12 @@ $end = $_POST['end'];
 
 // connection to the database
 try {
- $bdd = new PDO('mysql:host=localhost;dbname=fullcalendar', 'root', 'root');
- } catch(Exception $e) {
-exit('Unable to connect to database.');
+    $bdd = new PDO('mysql:host=localhost;dbname=fullcalendar', 'root', 'root');
+} catch (Exception $e) {
+    exit('Unable to connect to database.');
 }
- // update the records
+// update the records
 $sql = "UPDATE evenement SET title=?, start=?, end=? WHERE id=?";
 $q = $bdd->prepare($sql);
-$q->execute(array($title,$start,$end,$id));
+$q->execute(array($title, $start, $end, $id));
 ?>
